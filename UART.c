ffae56c9153f1db6db3d171ff736629483dcf7cb
paugd09 <pau09crz04gd99@gmail.com>
2023-01-27 14:27:28 -0600

    ahi va

--- UART.c
+++ UART.c
@@ -2,18 +2,19 @@
 
 extern void Configurar_UART2(void)
 {
-    SYSCTL->RCGCUART  = (1<<2);   //Paso 1 (RCGCUART) pag.344 UART/modulo0 0->Disable 1->Enable
+    SYSCTL->RCGCUART |= (1<<2);   //Paso 1 (RCGCUART) pag.344 UART/modulo0 0->Disable 1->Enable
     SYSCTL->RCGCGPIO |= (1<<3);     //Paso 2 (RCGCGPIO) pag.340 Enable clock port A
     //(GPIOAFSEL) pag.671 Enable alternate function
     GPIOD-> LOCK= 0X4C4F434B;
-    GPIOD-> CR = 0XF0; 
+    GPIOD-> CR = 0XC0; 
     GPIOD->AFSEL = (1<<6) | (1<<7);
     //GPIO Port Control (GPIOPCTL) PA0-> U0Rx PA1-> U0Tx pag.688
     GPIOD->PCTL = (GPIOD->PCTL&0xFFFFFF00) | 0x11000000;// (1<<0) | (1<<4);//0x00000011
     // GPIO Digital Enable (GPIODEN) pag.682
     GPIOD->DEN = (1<<6) | (1<<7);//PA1 PA0
     //UART0 UART Control (UARTCTL) pag.918 DISABLE!!
-    UART2->CTL = (0<<9) | (0<<8) | (0<<0);
+    UART2->CTL = (0<<0) | (0<<8) | (0<<09);
+    
 
     // UART Integer Baud-Rate Divisor (UARTIBRD) pag.914
     /*
@@ -31,6 +32,30 @@ extern void Configurar_UART2(void)
     UART2->CTL = (1<<0) | (1<<8) | (1<<9);
 
 
+extern void UART_2_CONFIG (void){
+    //Inicializacion del UART
+    // UART2 los PIN D6 Y D7 
+
+    SYSCTL -> RCGCUART |= 0X4;
+    SYSCTL -> RCGCGPIO |= 0x8;
+    GPIOD -> LOCK = 0x4C4F434B;
+    GPIOD -> CR = 0xC0; // los bit 6 y 7
+    GPIOD -> AFSEL = (1 << 6) | (1 << 7);
+    GPIOD -> PTCL |= (GPIOD -> PCTL & 0x00FFFFFF) | 0x11000000;
+    GPIOD -> DEN =(1 << 6) | (1 << 7);
+     //
+    //
+    UART2 -> CTL = (0 << 0 ) | (0 << 8) | ( 0 << 9);
+    //Porcion entera de BRD
+    UART2 -> IBRD =21;
+    //Porcion fracionaria de BRD
+    UART2 -> FBRD =45;
+    //
+    UART2 -> LCRH =(0x3 << 5) | (1<<4);
+    UART2 -> CC = 0; 
+    UART2 -> CTLC = (1 << 0) | (1 << 8) | (1 << 9);
+    
+}
 
 }
 
